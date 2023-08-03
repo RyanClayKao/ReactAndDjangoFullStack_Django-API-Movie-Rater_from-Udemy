a8d32e34885d6951022f01c12f098c74eb1bde92
RyanClayKao <f256987584@gmail.com>
2023-08-03 14:38:30 +0800

    完成自訂方法的更新、新增邏輯

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -22,8 +23,30 @@ class MovieViewSet(viewsets.ModelViewSet):
             movie = Movie.objects.get(id = pk)
             print("movie's title:", movie.title)
 
-            response = {'message': "it's working"}
-            return Response(response, status = status.HTTP_200_OK)
+            stars = request.data['stars']
+            print('stars:', stars)
+
+            # 目前這裡會輸出「user: AnonymousUser」，因為並沒有拿到真正登入者的資訊
+            # user = request.user
+            # print('user:', user)
+            # 如上述，因此暫且先 hardcode取得 User資料
+            user = User.objects.get(id=1)
+            print('user:', user.username) # 輸出「uesr: Ryan」 (當時設定的 superuser是 Ryan這個 user)
+
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': "Rating updated", 'result': serializer.data}
+                return Response(response, status = status.HTTP_200_OK)
+            except:
+                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': "Rating created", 'result': serializer.data}
+                return Response(response, status = status.HTTP_200_OK)
+            
         else:
             response = {'message': "You need to provide stars"}
             return Response(response, status = status.HTTP_400_BAD_REQUEST)
