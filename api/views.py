2f9758b86057268046440a191da945b95a45c4af
RyanClayKao <f256987584@gmail.com>
2023-08-03 11:23:10 +0800

    加入ViewSet的自訂方法

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
 
@@ -7,6 +9,11 @@ from .serializers import MovieSerializer, RatingSerializer
 class MovieViewSet(viewsets.ModelViewSet):
     serializer_class = MovieSerializer
     queryset = Movie.objects.all()
+
+    @action(detail = True, methods=['POST'])
+    def rate_movie(self, request, pk=None):
+        response = {'message': "it's working"}
+        return Response(response, status = status.HTTP_200_OK)
 
 
 class RatingViewSet(viewsets.ModelViewSet):
