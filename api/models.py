d01d220ea3a1a4488fe59452b2a5eb3443758da5
RyanClayKao <f256987584@gmail.com>
2023-08-03 10:08:56 +0800

    建立 Model並註冊至admin管理介面中

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=32)
+    description = models.TextField(max_length=360)
+
+class Rating(models.Model):
+    # 設定外來鍵
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # 設定數值的最小值及最大值範圍
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    class Meta:
+        # 設定 unique 組合，以確保不會重複
+        unique_together = (('user', 'movie'), )
+        index_together = (('user', 'movie'), )
