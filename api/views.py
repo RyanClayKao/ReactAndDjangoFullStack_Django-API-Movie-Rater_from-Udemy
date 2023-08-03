d5128c17a1e77f5c0cc6896fbbc16f09daad297d
RyanClayKao <f256987584@gmail.com>
2023-08-03 11:37:42 +0800

    在自訂方法中取用request的資料，進行判斷或是邏輯處理

--- api/views.py
+++ api/views.py
@@ -10,10 +10,24 @@ class MovieViewSet(viewsets.ModelViewSet):
     serializer_class = MovieSerializer
     queryset = Movie.objects.all()
 
+    # 備註：需搭配 postman軟體來測試 API
     @action(detail = True, methods=['POST'])
     def rate_movie(self, request, pk=None):
-        response = {'message': "it's working"}
-        return Response(response, status = status.HTTP_200_OK)
+        # 這裡的判斷僅是確認傳來後端時，是否有 stars這個欄位，但不會確認型別、數值範圍等等的，如果要做這些判斷需要再自己寫檢查的邏輯
+        if 'stars' in request.data:
+            # 印出前端 url中的 pk值 (例如：http://127.0.0.1:8000/api/movies/1/rate_movie/，pk即為 1)
+            print('pk:', pk)
+
+            # 使用 pk再去找 movie資料表的資料
+            movie = Movie.objects.get(id = pk)
+            print("movie's title:", movie.title)
+
+            response = {'message': "it's working"}
+            return Response(response, status = status.HTTP_200_OK)
+        else:
+            response = {'message': "You need to provide stars"}
+            return Response(response, status = status.HTTP_400_BAD_REQUEST)
+        
 
 
 class RatingViewSet(viewsets.ModelViewSet):
